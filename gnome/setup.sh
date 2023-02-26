57f8aae8c091da1670b23f6bba7da36ed77b13fa
muskit <15199219+muskit@users.noreply.github.com>
2023-02-26 05:44:17 -0800

    fix bad variable reference

--- gnome/setup.sh
+++ gnome/setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SCRIPT_DIR = $(dirname "$0")/scripts
+SCRIPT_DIR=$(dirname $(realpath "$0"))/scripts
 
 echo "<<<<<<<<<< [ GNOME Scripts ] >>>>>>>>>>"
 
 echo "------------ Installing Packages ------------"
-SCRIPT_DIR/install_packages.sh
+$SCRIPT_DIR/install_packages.sh
 
 echo "------ Creating links to config files ------"
-SCRIPT_DIR/make_links.sh
+$SCRIPT_DIR/make_links.sh
 
 # echo "------------ Creating autostarts ------------"
-# SCRIPT_DIR/create_autostarts.sh
+# $SCRIPT_DIR/create_autostarts.sh
 
 # echo "-------------- Running misc.sh --------------"
-# SCRIPT_DIR/misc.sh
+# $SCRIPT_DIR/misc.sh
 
 echo "-------- GNOME SETUP COMPLETE --------"
 cat postsetup_notes.txt
