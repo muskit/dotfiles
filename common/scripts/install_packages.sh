38966de8db98310ca960870cb6832f7a487cc4b8
muskit <15199219+muskit@users.noreply.github.com>
2023-02-26 17:56:20 -0800

    pipewire + effects, package installation split up

--- common/scripts/install_packages.sh
+++ common/scripts/install_packages.sh
@@ -11,5 +11,13 @@ fi
 
 pacman --noconfirm -Syu
 
-pacman --noconfirm -S libreoffice-fresh discord vlc obsidian libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitc5-configtool guake vim code python-pip
+# general applications
+pacman --noconfirm -S libreoffice-fresh discord vlc obsidian
+# system packages
+pacman --noconfirm -S libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitc5-configtool manjaro-pipewire guake
+# audioguake
+pacman --noconfirm -S pipewire-pulse pipewire-alsa easyeffects lsp-plugins
+# development packages
+pacman --noconfirm -S vim code python-pip
+
 pamac install --no-confirm noto-fonts-emoji-blob onedrive-abraunegg onedrivegui
--- common/scripts/misc.sh
+++ common/scripts/misc.sh
@@ -6,3 +6,6 @@ guake --restore-preferences $(dirname $0)/../dots/guake_preferences
 echo "Setting Git Author Variables..."
 git config --global user.email "15199219+muskit@users.noreply.github.com"
 git config --global user.name "muskit"
+
+echo "Installing EasyEffects presets (INTERACTIVE)..."
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/JackHack96/PulseEffects-Presets/master/install.sh)"
