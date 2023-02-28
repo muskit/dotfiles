5ebad5e22caf8a871badc4de752a9e1ddd33dad6
muskit <15199219+muskit@users.noreply.github.com>
2023-02-27 20:01:03 -0800

    add some packages

--- common/scripts/install_packages.sh
+++ common/scripts/install_packages.sh
@@ -12,12 +12,12 @@ fi
 pacman --noconfirm -Syu
 
 # general applications
-pacman --noconfirm -S libreoffice-fresh discord vlc obsidian spotify-launcher
-# system packages
-pacman --noconfirm -S libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitc5-configtool guake
+pacman --noconfirm -S libreoffice-fresh discord vlc obsidian spotify-launcher nextcloud-client
+# system-related packages
+pacman --noconfirm -S libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitc5-configtool guake tmux
 # audio
 pacman --noconfirm -S manjaro-pipewire pipewire-pulse pipewire-alsa easyeffects calf lsp-plugins
 # development packages
 pacman --noconfirm -S vim code python-pip
 
-pamac install --no-confirm noto-fonts-emoji-blob onedrive-abraunegg onedrivegui
+pamac install --no-confirm noto-fonts-emoji-blob onedrive-abraunegg onedrivegui indicator-stickynotes
