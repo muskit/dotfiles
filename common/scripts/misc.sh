3c02cd03f4643c08c128b4ede646fc6f6c9a90b1
muskit <15199219+muskit@users.noreply.github.com>
2023-03-11 22:30:47 -0800

    set editor to vim

--- common/scripts/misc.sh
+++ common/scripts/misc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "Setting Git Author Variables..."
+echo "Setting Git Variables..."
 git config --global user.email "15199219+muskit@users.noreply.github.com"
 git config --global user.name "muskit"
+git config --global core.editor vim
 
 echo "Installing EasyEffects presets (INTERACTIVE)..."
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/JackHack96/PulseEffects-Presets/master/install.sh)"
