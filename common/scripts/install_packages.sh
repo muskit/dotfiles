1a5cd6b00583cf396bd601f4007a60f140035ea1
muskit <15199219+muskit@users.noreply.github.com>
2023-05-06 01:18:24 -0700

    updates

--- common/scripts/install_packages.sh
+++ common/scripts/install_packages.sh
@@ -17,10 +17,11 @@ pacman --noconfirm -Syu
 # general applications
 pacman --noconfirm -S libreoffice-fresh discord vlc obsidian spotify-launcher nextcloud-client
 # system-related packages
-pacman --noconfirm -S libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitx5-configtool tmux guake
+pacman --noconfirm -S libpamac-flatpak-plugin fcitx5-qt fcitx5-gtk fcitx5-mozc fcitx5-unikey fcitx5-configtool tmux
 # development packages
 pacman --noconfirm -S vim python-pip github-cli patch base-devel
 # audio
 pacman -S manjaro-pipewire pipewire-pulse pipewire-alsa easyeffects calf lsp-plugins
 
-pamac install --no-confirm noto-fonts-emoji-blob onedrive-abraunegg onedrivegui indicator-stickynotes visual-studio-code-bin ocs-url
+pamac install --no-confirm noto-fonts-emoji-blob onedrive-abraunegg onedrivegui indicator-stickynotes visual-studio-code-bin ocs-url spotblock-git
+
