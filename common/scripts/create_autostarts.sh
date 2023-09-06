08f817568d962c34474018ccf6b88204b21d551d
muskit <15199219+muskit@users.noreply.github.com>
2023-09-06 15:19:18 -0700

    create symbolic link for nvim

--- common/scripts/create_autostarts.sh
+++ common/scripts/create_autostarts.sh
@@ -3,6 +3,5 @@
 START_DIR=$HOME/.config/autostart
 
 mkdir -p $START_DIR
-cp -i /usr/share/applications/guake.desktop $START_DIR/
 cp -i /usr/share/applications/org.fcitx.Fcitx5.desktop $START_DIR/
 cp -i /usr/share/applications/onedrivegui.desktop $START_DIR/
--- common/scripts/make_links.sh
+++ common/scripts/make_links.sh
@@ -6,3 +6,4 @@ ln -si $DOTS_FOLDER/.vimrc $HOME/.vimrc
 
 ln -si $DOTS_FOLDER/.config/fcitx5 $HOME/.config/
 ln -si $DOTS_FOLDER/.config/environment.d $HOME/.config/
+ln -si $DOTS_FOLDER/.config/nvim $HOME/.config/
